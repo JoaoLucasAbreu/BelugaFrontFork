700c0642a2651879a5bd92d5a723aab9e6d99280
felipefurukawa <felpissss@gmail.com>
2024-09-13 15:18:55 -0300

    dashboard

--- frontend/app/ui/dashboard/card/card.jsx
+++ frontend/app/ui/dashboard/card/card.jsx
@@ -6,17 +6,14 @@ const Card = ({ item }) => {
     <div className={styles.container}>
       <MdSupervisedUserCircle size={24} />
       <div className={styles.texts}>
-        <span className={styles.title}>{item.title}</span>
-        <span className={styles.number}>{item.number}</span>
+        <span className={styles.title}>Total Users</span>
+        <span className={styles.number}>30.401</span>
         <span className={styles.detail}>
-          <span className={item.change > 0 ? styles.positive : styles.negative}>
-            {item.change}%
-          </span>{" "}
-          {item.change > 0 ? "more" : "less"} than previous week
+          <span className={styles.positive}>12%</span> more than previous
         </span>
       </div>
     </div>
-  );
+  );  
 };
 
 export default Card;
