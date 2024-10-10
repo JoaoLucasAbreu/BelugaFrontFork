f1d85e77b3fc0cd1f1be9d1cff259114d42ab838
felipefurukawa <felpissss@gmail.com>
2024-10-10 14:14:32 -0300

    commit do gráfico

--- frontend/app/ui/dashboard/chart/chart.jsx
+++ frontend/app/ui/dashboard/chart/chart.jsx
@@ -1,69 +1,51 @@
 "use client"
 
 import styles from './chart.module.css'
-import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, ComposedChart } from 'recharts';
 
 const data = [
-  {
-    name: "Sun",
-    visit: 4000,
-    click: 2400,
-  },
-  {
-    name: "Mon",
-    visit: 3000,
-    click: 1398,
-  },
-  {
-    name: "Tue",
-    visit: 2000,
-    click: 3800,
-  },
-  {
-    name: "Wed",
-    visit: 2780,
-    click: 3908,
-  },
-  {
-    name: "Thu",
-    visit: 1890,
-    click: 4800,
-  },
-  {
-    name: "Fri",
-    visit: 2390,
-    click: 3800,
-  },
-  {
-    name: "Sat",
-    visit: 3490,
-    click: 4300,
-  },
+  { name: 'Jan', Gastos: 49998, Minutos: 4503.31},
+  { name: 'Fev', Gastos: 43120, Minutos: 3902.12 },
+  { name: 'Mar', Gastos: 37145, Minutos: 3100.56 },
+  { name: 'Abr', Gastos: 29143, Minutos: 2340.30 },
+  { name: 'Mai', Gastos: 23180, Minutos: 1840.97 },
+  { name: 'Jun', Gastos: 27902, Minutos: 2003.13 },
+  { name: 'Jul', Gastos: 36955, Minutos: 3992.12 },
+  { name: 'Ago', Gastos: 20, Minutos: 80 },
+  { name: 'Set', Gastos: 35, Minutos: 85 },
+  { name: 'Out', Gastos: 45, Minutos: 90 },
+  { name: 'Nov', Gastos: 50, Minutos: 110 },
+  { name: 'Dez', Gastos: 35, Minutos: 95 },
 ];
 
 const Chart = () => {
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>Weekly Recap</h2>
-      <ResponsiveContainer width="100%" height="90%">
-        <LineChart
-          width={500}
-          height={300}
+      <h2 className={styles.title}>Gastos por Minutos traduzidos</h2>
+      <div>
+      <p className={styles.bigNumber}>Total Gasto: <b>500K</b></p>
+      <p className={styles.bigNumber}>Minutos Traduzidos: <b>460</b></p>
+      
+      </div>
+      <ResponsiveContainer width="100%" height={392}>
+        <ComposedChart
           data={data}
           margin={{
-            top: 5,
-            right: 30,
+            top: 20,
+            right: 20,
+            bottom: 20,
             left: 20,
-            bottom: 5,
           }}
         >
+          <CartesianGrid stroke="#f5f5f5" />
           <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip contentStyle={{background:"#151c2c", border:"none"}}/>
+          <YAxis yAxisId="left" orientation="left" />
+          <YAxis yAxisId="right" orientation="right" />
+          <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="visit" stroke="#8884d8" strokeDasharray="5 5" />
-          <Line type="monotone" dataKey="click" stroke="#82ca9d" strokeDasharray="3 4 5 2" />
-        </LineChart>
+          <Bar dataKey="Gastos" barSize={20} fill="#82ca9d" yAxisId="left"/>
+          <Line type="monotone" dataKey="Minutos" stroke="#8884d8" yAxisId="right"/>
+        </ComposedChart>
       </ResponsiveContainer>
     </div>
   )
