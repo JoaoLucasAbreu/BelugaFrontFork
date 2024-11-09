0cda413944842b4827aef8081d27de62bca197af
João Lucas Abreu <joaolucasabreunogueira@gmail.com>
2024-11-09 02:00:00 -0300

    feat: more changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    trailingSlash: true,
+    reactStrictMode: true,
+    output: 'standalone',
+    async redirects() {
+        return [
+            // Home page to time tracker
+            {
+                source: '/',
+                destination: '/dashboard',
+                permanent: false,
+            },
+        ]
+    },
+}
 
 export default nextConfig;
